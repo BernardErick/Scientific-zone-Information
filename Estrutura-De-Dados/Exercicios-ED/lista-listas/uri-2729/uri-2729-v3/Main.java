09e0fe054140b5a605cf309d36da345651c1c9f6
Bernard Erick <49220123+BernardErick@users.noreply.github.com>
2021-03-18 13:29:42 -0300

    Add files via upload

---
+++ Estrutura-De-Dados/Exercicios-ED/lista-listas/uri-2729/uri-2729-v3/Main.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        scanner.nextLine();
+        for(int i = 0; i < N;i++){
+            ListaDinamicaOrdenada list = new ListaDinamicaOrdenada();
+            String items = scanner.nextLine();
+            String[] item = items.split(" ");
+            for(int j = 0; j < item.length;j++){
+                if(!list.contem(item[j]))
+                    list.addOrdenado(item[j]);
+            };
+            StringBuilder builder = new StringBuilder();
+            for(int j = 0; j < list.tamanho();j++)
+                builder.append(list.get(j)+" ");
+            System.out.println(builder.toString().trim());
+        }
+
+    }
+}
---
+++ Estrutura-De-Dados/Exercicios-ED/lista-listas/uri-2729/uri-2729-v3/No.java
@@ -0,0 +1,8 @@
+public class No {
+    public String dado;
+    public No proximo;
+
+    public No(String dado) {
+        this.dado = dado;
+    }
+}
